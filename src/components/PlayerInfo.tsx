8b039ef964e2eb0bfdf7a61bd92d65c20a55fddb
Subir-Droid <subirdeyabhi@gmail.com>
2024-07-16 19:07:55 +0530

    updating states in playerInfo

--- src/components/PlayerInfo.tsx
+++ src/components/PlayerInfo.tsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PlayerInfo = ({name , symbol}) =>{
+
+    const [isEditing , setisEditing] = useState(false)
+
+    const [playerName , setPlayername] = useState(name)
+
+    const handleEditClick = () =>{
+        setisEditing((editable) => !editable)
+    }
+
+    const updateName = (event) =>{
+        console.log(event.target.value)
+        setPlayername(event.target.value)
+    }
+
+    let playernameOrInput = <span className="player-name">{playerName}</span>
+
+    if(isEditing){
+        playernameOrInput = <input type="text" required value={playerName} onChange={updateName}/>
+    }
 
     return(
         <li>
               <span className="players">
-              <span className="player-name">{name}</span>
+                {playernameOrInput}
               <span className="player-symbol">{symbol}</span>
               </span>
-              <button>Edit</button>
+              <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+              
             </li>
     )
 
